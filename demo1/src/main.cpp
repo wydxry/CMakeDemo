07d9d645828a5f7d2e64cbf6d9a9749a9a0cbd56
Li Zeng <wydxry@163.com>
2024-12-01 17:14:07 +0800

    Add files via upload

---
+++ demo1/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    std::cout << "Hello, CMake!" << std::endl;
+	std::cout << "Hello, world!" << std::endl;
+	system("pause");
+    return 0;
+}
